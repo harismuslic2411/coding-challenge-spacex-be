15bee2de3b3d3ae94f474cacb45f09052fc27def
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2023-06-19 07:23:09 +0200

    move code to indexjs

--- src/constants/constants.js
+++
@@ -1,9 +0,0 @@
-'use-strict';
-
-const UrlConstants = {
-    PORT: 4200,
-    SERVER_URL: 'http://localhost:4200',
-    FRONTEND_URL: 'http://localhost:3000',
-};
-
-exports.UrlConstants = UrlConstants;
