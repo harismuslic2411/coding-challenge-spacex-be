5dd06d14c4b6dc9fc5c4e1b8253399ec17c7dd8f
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2023-06-19 07:55:24 +0200

    add statuses and messages to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,4 +7,24 @@ const UrlConstants = {
     SPACEX_URL: 'https://api.spacexdata.com/v5/launches'
 };
 
+const Statuses = {
+    SUCCESS_GET: 200,
+    SUCCESS_POST: 201,
+    NO_IMAGES: 404, // resource not found
+    ERROR_GET: 500, //set default to 500
+}
+
+const Messages = {
+    SUCCESS_GET: 'Data succesfully retrieved',
+    SUCCESS_POST: 'Data succesfully posted',
+    NO_IMAGES: 'Resource not found',
+    ERROR_GET: 'Something went wront, please try again'
+}
+
+module.exports = {
+    UrlConstants,
+    Statuses,
+    Messages
+}
+
 exports.UrlConstants = UrlConstants;
