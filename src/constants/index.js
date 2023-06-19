f3971ddd6b750b8ad4a5ff9fe208e1a259771afd
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2023-06-19 07:34:54 +0200

    add spacex base url

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,7 @@ const UrlConstants = {
     PORT: 4200,
     SERVER_URL: 'http://localhost:4200',
     FRONTEND_URL: 'http://localhost:3000',
+    SPACEX_URL: 'https://api.spacexdata.com/v5/launches'
 };
 
 exports.UrlConstants = UrlConstants;
