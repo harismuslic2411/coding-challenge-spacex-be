22278b45104a459ba50f63f5afe2bdd472211c83
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2023-06-19 07:38:42 +0200

    add route to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { getSpaceXLaunches } = require('../controllers/SpaceX/get');
 
 const routes = (app) => {
+
+    router.get('/spacex/launch', getSpaceXLaunches);
   
     app.use(router);
 };
